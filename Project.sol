1988e3e880bc1ccf20ad7321dc51063bc463287b
cyrillicw <cyrillicw@gmail.com>
2021-01-25 14:29:07 +0200

    Project.col

--- Project.sol
+++ Project.sol
@@ -7,6 +7,7 @@ import "./Voting.sol";
 import "./Porgi.sol";
 
 import "https://github.com/aave/protocol-v2/blob/master/contracts/misc/WETHGateway.sol";
+import "https://github.com/aave/protocol-v2/blob/master/contracts/dependencies/openzeppelin/contracts/SafeMath.sol";
 import "https://github.com/de-porgi/minime/blob/master/contracts/MiniMeToken.sol";
 import "https://github.com/aave/protocol-v2/blob/master/contracts/dependencies/openzeppelin/contracts/ERC20.sol";
 
@@ -14,6 +15,8 @@ import "https://github.com/aave/protocol-v2/blob/master/contracts/dependencies/o
  * @title Project
  */
 contract Project is MiniMeToken {
+
+    using SafeMath for uint256;
     
     struct Serie {
         uint32 Duration;
@@ -65,7 +68,7 @@ contract Project is MiniMeToken {
         changeController(address(this));
     }
     
-  	receive() external payable {}
+    receive() external payable {}
     
     function _makeDeposit(uint256 amount) private {
         require(address(this).balance >= amount, "Not Enough ETH to Deposit");
@@ -76,5 +79,33 @@ contract Project is MiniMeToken {
         ERC20 aWETH = ERC20(_DepositManager.getAWETHAddress());
         require(aWETH.approve(_DEPOSIT_ADDRESS, amount), "Not Enough aWETH to Withdraw");
         _DepositManager.withdrawETH(amount, address(this));
+    }
+	
+	    function GetETHBalance() public view returns (uint256) {
+        ERC20 aWETH = ERC20(_DepositManager.getAWETHAddress());
+        return aWETH.balanceOf(address(this));
+    }
+    
+    function withdrawETH() public payable {
+        // project is closed TODO Add corresponding variable
+        require(false, "Project Is Not Closed");
+        uint256 totalEth = GetETHBalance();
+        uint256 totalSupply = totalSupply();
+        uint256 senderTokens = balanceOf(msg.sender);
+        uint256 ETHToWithdraw =  totalEth.mul(senderTokens).div(totalSupply);
+        destroyTokens(msg.sender, senderTokens);
+        _safeTransferETH(msg.sender, ETHToWithdraw);
+    }
+    
+    function _safeTransferETH(address to, uint256 value) private {
+        (bool success, ) = to.call{value: value}(new bytes(0));
+        require(success, 'ETH_TRANSFER_FAILED');
+    }
+    
+    function _unlockETH(uint8 numerator, uint8 denominator) private {
+        uint256 balance = GetETHBalance();
+        uint256 toUnlock = balance.mul(numerator).div(denominator);
+        _withdrawDeposit(toUnlock);
+        _safeTransferETH(msg.sender, toUnlock);
     }
 }
